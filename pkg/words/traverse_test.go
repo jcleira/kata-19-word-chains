7f60571c07b09410cc32989304f4ec7fb3a2e8a9
jcleira <jmc.leira@gmail.com>
2019-07-21 22:32:31 +0200

    Add Traverse struct to perform traversal searching
    This commit adds the Traverse struct & methods, this struct is intended
    to perform traversal searchs on the LinkedWords to discover word chains
    betweeen the start & end words given.

---
+++ pkg/words/traverse_test.go
@@ -0,0 +1,63 @@
+package words
+
+import (
+	"errors"
+	"reflect"
+	"testing"
+)
+
+func TestPerform(t *testing.T) {
+	startWord := &Word{
+		Term: "foo",
+		LinkedWords: []*Word{
+			&Word{
+				Term: "foe",
+				LinkedWords: []*Word{
+					&Word{Term: "fee"},
+				},
+			},
+		},
+	}
+
+	tests := []struct {
+		Description   string
+		StartWord     *Word
+		EndWord       *Word
+		ExpectedError error
+		ExpectedChain []string
+	}{
+		{
+			Description:   "when Traverse succesfully finds a path for the words",
+			StartWord:     startWord,
+			EndWord:       &Word{Term: "fee"},
+			ExpectedChain: []string{"foo", "foe", "fee"},
+		},
+		{
+			Description:   "when traverse fails to find a path between the words",
+			StartWord:     startWord,
+			EndWord:       &Word{Term: "feo"},
+			ExpectedError: errors.New("error, no chain found between 'foo' and 'feo'"),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.Description, func(t *testing.T) {
+			traverse := &Traverse{
+				StartWord: test.StartWord,
+				EndWord:   test.EndWord,
+				Results:   make(chan Chain),
+			}
+
+			chain, err := traverse.Perform()
+
+			if !reflect.DeepEqual(test.ExpectedError, err) {
+				t.Fatalf("expected err to be %v, got %v", test.ExpectedError, err)
+			}
+
+			if !reflect.DeepEqual(test.ExpectedChain, chain) {
+				t.Fatalf("expected chain to be %v, got %v", test.ExpectedChain, chain)
+			}
+		})
+	}
+
+}
