064305259a110a380e38186f39ba2845d5d7e716
jcleira <jmc.leira@gmail.com>
2019-07-21 10:49:57 +0200

    Add Word linking feature to store word linked words
    This commit adds the Words linking feature so we would be able to store
    for each Word it's Linked Words (on a LinkedWords attribute) we would be
    using that LinkedWords to traverse search the whole list.

--- words/word.go
+++ words/word.go
@@ -2,5 +2,59 @@ package words
 
 // Word represents an unique real world word.
 type Word struct {
-	Term string
+	Term        string
+	LinkedWords []Word
+}
+
+// Link would seek linked words for the given word on a word's dictionary.
+//
+// A linkable word should match on char length and just have one letter different
+// from the given word.
+//
+// TODO: Link should cleanup any previous Link call.
+func (w *Word) Link(words []Word) {
+	for i := 0; i < len(words); i++ {
+		if w.isLinkable(words[i]) {
+			w.LinkedWords = append(w.LinkedWords, words[i])
+		}
+	}
+}
+
+// isLinkable will check if a given word is linkable or not with the current
+// word.
+//
+// A linkable word should match on char length and just have one letter different
+// from the given word.
+//
+// Examples of linkable words for foo:
+// - boo, foe, feo, too
+//
+// Examples of non-linkable words for foo:
+// - foo, bar, faa, foobar, barfoo
+//
+// Returns true if the given word is linkable, false otherwise.
+func (w *Word) isLinkable(word Word) bool {
+	if w.Term == word.Term {
+		return false
+	}
+
+	if len(w.Term) != len(word.Term) {
+		return false
+	}
+
+	differences := 0
+	for i := 0; i < len(w.Term); i++ {
+		if w.Term[i] == word.Term[i] {
+			continue
+		}
+
+		if differences == 1 {
+			return false
+		} else {
+			differences++
+		}
+	}
+
+	return true
+
 }
